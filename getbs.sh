b2e8b1e03ae8d5a829398a62cdcb7c71bfdf7cb1
czoczo <czoczo@ant.meadow.eu.org>
2017-12-27 16:29:37 +0100

    test

--- getbs.sh
+++ getbs.sh
@@ -13,6 +13,7 @@ install () {
 esac
 }
 
+#test
 #command -v zsh >/dev/null 2>&1 || install
 #[[ $EUID -ne 0 ]] && sudo usermod -s /bin/zsh $USER || usermod -s /bin/zsh $USER
 #wget cz0.cz/chozsh-latest.tar.gz -q -O - | tar --owner=$UID --group=$GID --no-same-owner -xz -C ~
