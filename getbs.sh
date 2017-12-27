0f390260ec1d361a1d071d6ced45d4220c184ce0
czoczo <czoczo@ant.meadow.eu.org>
2017-12-27 16:31:52 +0100

    Path fix

--- getbs.sh
+++ getbs.sh
@@ -13,12 +13,11 @@ install () {
 esac
 }
 
-#test
 #command -v zsh >/dev/null 2>&1 || install
 #[[ $EUID -ne 0 ]] && sudo usermod -s /bin/zsh $USER || usermod -s /bin/zsh $USER
 #wget cz0.cz/chozsh-latest.tar.gz -q -O - | tar --owner=$UID --group=$GID --no-same-owner -xz -C ~
 wget cz0.cz/bettershell-latest.tar.gz -q -O - | tar -xz -C ~
 #echo -e 'export SHELL=`which zsh`'"\n"'[ -z "$ZSH_VERSION" ] && exec "$SHELL" -l' > ~/.profile
 
-CMD='[ -f ~/bshell/bb.sh ] && . ~/bshell/bb.sh'
+CMD='[ -f ~/.bshell/bb.sh ] && . ~/.bshell/bb.sh'
 grep -q "$CMD" ~/.bashrc || echo "$CMD" >> ~/.bashrc
