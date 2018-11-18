a334da1a311251137902b69938f5f51692034519
czoczo <mr.czoczo@gmail.com>
2018-11-19 00:06:04 +0100

    installation bug fix

--- getbb.sh
+++ getbb.sh
@@ -2,4 +2,4 @@
 wget cz0.cz/betterbash-latest.tar.gz -q -O - | tar -xz --no-same-owner -C ~
 CMD='[ -f ~/.bshell/bb.sh ] && . ~/.bshell/bb.sh'
 grep -q "$CMD" ~/.bashrc || echo "$CMD" >> ~/.bashrc
-source ~/.bashrc
+. ~/.bashrc
