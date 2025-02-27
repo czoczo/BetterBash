e9aa76e640ad768372d66008b94eb66acec3d647
czoczo <mr.czoczo@gmail.com>
2025-02-27 23:45:31 +0100

    updated README.md

--- README.md
+++ README.md
@@ -15,11 +15,5 @@ source ~/.bashrc
 ```
 or just:
 ```
-curl https://cz0.cz/getbb.sh | sh && source ~/.bashrc
+curl https://git.cz0.cz/getbb.sh | sh && source ~/.bashrc
 ```
-(but only if you really, really, _really_ trust me)
-
-
-
-
-(in fact, no one really should... one of the many reasones being https://www.idontplaydarts.com/2016/04/detecting-curl-pipe-bash-server-side/ )
--- getbb.sh
+++ getbb.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-wget cz0.cz/betterbash-latest.tar.gz -q -O - | tar -xz --no-same-owner -C ~
-CMD='[ -f ~/.bshell/bb.sh ] && . ~/.bshell/bb.sh'
-grep -q "$CMD" ~/.bashrc || echo "$CMD" >> ~/.bashrc
+URL='https://git.cz0.cz/czoczo/BetterBash/raw/branch/master/.bshell' && \
+DIR=~/.bshell && \
+CMD='[ -f ~/.bshell/bb.sh ] && . ~/.bshell/bb.sh' && \
+mkdir -p $DIR && \
+wget $URL/bb.sh -O $DIR/bb.sh && \
+wget $URL/git-prompt.sh -O $DIR/git-prompt.sh && \
+grep -q "$CMD" ~/.bashrc || echo "$CMD" >> ~/.bashrc && \
+source ~/.bashrc
+
