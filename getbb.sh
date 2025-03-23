1ebb891cb418649d88d1d9a8cc398261e195bd72
czoczo <mr.czoczo@gmail.com>
2025-03-24 00:40:26 +0100

    work on install method

--- getbb.sh
+++ getbb.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+
 get_file() {
-  echo -e "GET /czoczo/BetterBash/raw/branch/master/$1 HTTP/1.1\r\nHost: git.cz0.cz\r\nConnection: close\r\n\r\n" \
-  | openssl s_client -quiet -connect cz0.cz:443 2>/dev/null \
+  HOST="git.cz0.cz"
+  echo -e "GET /czoczo/BetterBash/raw/branch/master/$1 HTTP/1.1\r\nHost: $HOST\r\nConnection: close\r\n\r\n" \
+  | openssl s_client -quiet -connect $HOST:443 2>/dev/null \
   | sed '1,/^\r$/d'
 }
 
-DIR=~/.bb && \
-CMD="[ -f $DIR/bb.sh ] && . $DIR/bb.sh" && \
+DIR=~/.bb
+CMD=$(cat <<-END
+# BetterBash
+[ -f $DIR/bb.sh ] && . $DIR/bb.sh
+END
+)
+
 mkdir -p $DIR && \
 get_file /prompt/bb.sh > $DIR/bb.sh && \
 get_file /prompt/git-prompt.sh > $DIR/git-prompt.sh && \
-get_file /.inputrc >> ~/.inputrc && \
-grep -q "$CMD" ~/.bashrc || echo -e "# BetterBash\n$CMD" >> ~/.bashrc
+grep -q "BetterBash" ~/.inputrc || get_file /.inputrc >> ~/.inputrc && \
+bind -f ~/.inputrc && \
+grep -q "BetterBash" ~/.bashrc || echo "$CMD" >> ~/.bashrc
