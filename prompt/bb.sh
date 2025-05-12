d382bb4dfb4640ebb51ec95054bd00339fdb90c8
czoczo <mr.czoczo@gmail.com>
2025-05-12 12:34:55 +0200

    colors rename

--- prompt/bb.sh
+++ prompt/bb.sh
@@ -11,7 +11,8 @@ function getChar {
   colfg=$(( n % 8 )) && n=$(( n / 8 ))
   colbg=$(( n % 8 ))
   [[ "$1" -eq 1 && ( "$char" -eq 1 || "$char" -eq 3 ) ]] && char=$(( (char + 2) % 4 )) # mirror horizontal arrows
-  echo -en "\[\033[1;${arrfg[$colfg]};${arrbg[$colbg]}m${arrchar[$char]}\]"
+  #echo -en "\[\033[1;${arrfg[$colfg]};${arrbg[$colbg]}m${arrchar[$char]}\]"
+  echo -en "\[\033[1;${arrfg[$colfg]}m${arrchar[$char]}\]"
 }
 
 function hashColor {
@@ -46,21 +47,17 @@ HBAR="─"
 PR_ULCORNER="┌"
 PR_LLCORNER="└"
 
-GREEN='\[\033[00;92m\]'
-GREENB='\[\033[00;92;1m\]'
-BLUE='\[\033[00;36;1m\]'
-MAGENTA='\[\033[00;95;1m\]'
-GREY='\[\033[00;90m\]'
-YELLOW='\[\033[00;93m\]'
-YELLOWB='\[\033[00;93;1m\]'
-RED='\[\033[00;31m\]'
-REDB='\[\033[00;31;1m\]'
-WHITE='\[\033[00;97;1m\]'
+PRIMARY_COLOR='\[\033[00;92m\]'
+SECONDARY_COLOR='\[\033[00;95;1m\]'
+ROOT_COLOR='\033[00;41;97;1m'
+TIME_COLOR='\[\033[00;93;1m\]'
+ERR_COLOR='\[\033[00;31;1m\]'
 WHITEB='\[\033[00;97;1m\]'
 RST='\[\033[0m\]'
 BOLD='\[\033[1m\]'
 BORDCOL='\[\033[00;90;1m\]'
-USERCOL=$MAGENTA
+USERCOL=$SECONDARY_COLOR
+PATH_COLOR=$WHITEB
 
 export GIT_PS1_SHOWCOLORHINTS=true
 export GIT_PS1_SHOWDIRTYSTATE=true
@@ -73,16 +70,16 @@ function __prompt_command() {
   local RETURN_CODE="$?"
   PS1=""
   # Handling returne code
-  RCOL="$GREEN"
+  RCOL="${PRIMARY_COLOR}"
   EXIT="$HBAR$HBAR$HBAR$HBAR$HBAR"
   if [[ $RETURN_CODE != 0 ]]; then
-     EXIT="$WHITEB($REDB$RETURN_CODE ↵$WHITEB)"
-     RCOL="$REDB"
+     EXIT="$WHITEB(${ERR_COLOR}$RETURN_CODE ↵$WHITEB)"
+     RCOL="${ERR_COLOR}"
   fi
 
   USER=$(whoami)
   if [ $UID -eq "0" ]; then
-     USERCOL='\033[00;41;97;1m'
+     USERCOL=$ROOT_COLOR
      USER="${USER^^}"
   fi
 
@@ -90,16 +87,16 @@ function __prompt_command() {
   PROCCNT=$(jobs -p 2>/dev/null | wc -l )
   if [ "$PROCCNT" -ne "0" ]; then
     #BGPROCCOL='\033[1;95;5m'
-    BGPROCCOL="$BORDCOL$HBAR$HBAR$WHITEB($MAGENTA\j ↻$WHITEB)"
+    BGPROCCOL="$BORDCOL$HBAR$HBAR$WHITEB(${SECONDARY_COLOR}\j ↻$WHITEB)"
   fi
 
   HOSTNAM="$(cat /etc/hostname)"
 
-  GITPROMPT=$(__git_ps1 " on$GREEN %s")
+  GITPROMPT=$(__git_ps1 " on${PRIMARY_COLOR} %s")
 
-  LEFT="\n$BORDCOL\[\016\]$PR_ULCORNER$HBAR\[\017\]$WHITEB($USERCOL$USER$WHITEB@$GREEN\h:$cur_tty$WHITEB)$BORDCOL$HBAR$HBAR$WHITEB($CH$WHITEB)$BGPROCCOL"
+  LEFT="\n$BORDCOL\[\016\]$PR_ULCORNER$HBAR\[\017\]$WHITEB($USERCOL$USER$WHITEB@${PRIMARY_COLOR}\h:$cur_tty$WHITEB)$BORDCOL$HBAR$HBAR$WHITEB($CH$WHITEB)$BGPROCCOL"
 
-  RIGHT="$EXIT$BORDCOL$HBAR$HBAR$HBAR$WHITEB($YELLOWB\d$WHITEB)$BORDCOL$HBAR$HBAR$HBAR$WHITEB($RCOL\t$WHITEB)$BORDCOL$HBAR$HBAR$HBAR$HBAR\n$BORDCOL\[\016\]$PR_LLCORNER\[\017\]$BORDCOL$HBAR$WHITEB(\w)$BORDCOL$HBAR$WHITEB($GREEN\\\$$RST$GITPROMPT$WHITEB)$BORDCOL-> \[\e[0m\]"
+  RIGHT="$EXIT$BORDCOL$HBAR$HBAR$HBAR$WHITEB($TIME_COLOR\d$WHITEB)$BORDCOL$HBAR$HBAR$HBAR$WHITEB($RCOL\t$WHITEB)$BORDCOL$HBAR$HBAR$HBAR$HBAR\n$BORDCOL\[\016\]$PR_LLCORNER\[\017\]$BORDCOL$HBAR$WHITEB(${PATH_COLOR}\w${WHITE})$BORDCOL$HBAR$WHITEB(${PRIMARY_COLOR}\\\$$RST$GITPROMPT$WHITEB)$BORDCOL-> \[\e[0m\]"
 
   L_LEN="$USER$HOSTNAM$CH\j"
   R_LEN="XXX XXX XX, XX:XX:XX$RETURN_CODE"
